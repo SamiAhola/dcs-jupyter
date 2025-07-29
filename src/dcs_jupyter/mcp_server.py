a90c8ea32f89f754a272b839625e08ff231f9d98
Sami Ahola <sami.ahola@iki.fi>
2025-07-29 22:11:34 +0300

    Add rest of the tools in MCP server.

--- src/dcs_jupyter/mcp_server.py
+++ src/dcs_jupyter/mcp_server.py
@@ -9,12 +9,25 @@ except ImportError:
     sys.exit(1)
 
 from dcs_jupyter.connection import DCSConnection
-from dcs_jupyter.tools import create_tool_execute_lua
+from dcs_jupyter.tools import (
+    create_tool_execute_lua,
+    create_tool_get_airbases,
+    create_tool_spawn_aircraft,
+    create_tool_get_mission_info,
+)
 
 
 def main() -> None:
     dcs = DCSConnection()
-    mcp: FastMCP = FastMCP(name='dcs-world-mcp', tools=[create_tool_execute_lua(dcs)])
+    mcp: FastMCP = FastMCP(
+        name='dcs-world-mcp',
+        tools=[
+            create_tool_execute_lua(dcs),
+            create_tool_get_airbases(dcs),
+            create_tool_spawn_aircraft(dcs),
+            create_tool_get_mission_info(dcs),
+        ],
+    )
 
     """Run the MCP server."""
     print('Starting DCS World MCP Server...')
